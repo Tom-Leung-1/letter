42dbf8d74006b72376f8871d10262d1ccba79183
Tom-Leung-bb <wfleung8@cse.cuhk.edu.hk>
2021-05-20 19:29:39 +0800

    navbar added

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [ // export so other file can access || const map array item to store menu items
+    {
+        title: 'Home',
+        url: "#", // url
+        cName: 'nav-links' // class name so you don't have to manually type it 
+    },
+    {
+        title: 'Services',
+        url: "#",
+        cName: 'nav-links'
+    },
+    {
+        title: 'Products',
+        url: "#",
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: "#",
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: "#",
+        cName: 'nav-links-mobile'
+    },
+]
